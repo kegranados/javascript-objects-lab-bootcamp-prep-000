34b36b203a380fe73307445aac7496635d9b65bd
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-23 02:50:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ function deleteFromObjectByKey(object, key) {
   delete object1[key]
   return object1
 }
-
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
