2e51c5c4fd21a54bcaa3c042917ba6908cbd4168
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-23 03:08:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object[key]
+  delete Object.assign({}, object, {[key]: value})
   return object
 }
 function deleteFromObjectByKey(object, key) {
